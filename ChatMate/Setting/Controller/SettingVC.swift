8b60a240ffbfec363e4c57110f5d0c170ee3b191
Saboor Ahmad <saboorahm7e@gmail.com>
2026-01-07 17:03:53 +0500

    setting screen ui and basic navigation flow

---
+++ ChatMate/Setting/Controller/SettingVC.swift
@@ -0,0 +1,50 @@
+//
+//  SettingVC.swift
+//  ChatMate
+//
+//  Created by saboor on 07/01/2026.
+//
+
+import UIKit
+
+enum ProvidedLanguage: String {
+    case en = "English"
+    case ur = "Urdu"
+}
+
+class SettingVC: UIViewController {
+    
+    var currentLanguage : ProvidedLanguage = .en
+
+    @IBOutlet weak var languageLabel: UILabel!
+    
+    @IBOutlet weak var languageMenuBtn: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+       title = "Settings"
+    
+        languageMenuBtn.showsMenuAsPrimaryAction = true
+        
+        let english = UIAction(title: "English") {  _ in
+            self.currentLanguage = .en
+            self.languageLabel.text = self.currentLanguage.rawValue
+        }
+        let urdu = UIAction(title: "Urdu") { _ in
+            self.currentLanguage = .ur
+            self.languageLabel.text = self.currentLanguage.rawValue
+        }
+        
+        languageMenuBtn.menu = UIMenu(children: [english,urdu]
+        )
+    }
+    @IBAction func darkModeToggle(_ sender: UISwitch) {
+        
+    }
+    
+    @IBAction func singOutTap(_ sender: Any) {
+        DataManager.shared.isAuthenticate = false
+        let rootVC = VCFactory.makeRootVC()
+        UIApplication.sceneDelegate?.setRootVC(rootVC, animated: true)
+    }
+}
